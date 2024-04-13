ca8b9ed31319a705a13a492342090691570b9dc9
Nikita Savin <klaster26@mail.ru>
2024-04-13 14:08:48 +0300

    feat: добавить обработку ошибок http

--- app.py
+++ app.py
@@ -1,33 +1,57 @@
 from flask import Flask, render_template
-from data import title, subtitle, description, departures, tours, background
 
+from data import background, departures, description, subtitle, title, tours
 
 app = Flask(__name__)
 
 
-@app.route('/departures/<direction>')
+@app.route("/departures/<direction>")
 def render_departure(direction):
     directions = {}
     for tour in tours:
-        if tours[tour]['departure'] == direction:
+        if tours[tour]["departure"] == direction:
             directions[tour] = tours[tour]
-    return render_template('departure.html', departure=departures[direction],
-                           tours=tours, directions=directions, departures=departures,
-                           title=title)
+    return render_template(
+        "departure.html",
+        departure=departures[direction],
+        tours=tours,
+        directions=directions,
+        departures=departures,
+        title=title,
+    )
 
 
-@app.route('/')
+@app.route("/")
 def render_index():
-    return render_template('index.html', description=description,
-                           departures=departures, tours=tours, title=title,
-                           background=background, subtitle=subtitle)
+    return render_template(
+        "index.html",
+        description=description,
+        departures=departures,
+        tours=tours,
+        title=title,
+        background=background,
+        subtitle=subtitle,
+    )
 
 
-@app.route('/tours/<tour>')
+@app.route("/tours/<tour>")
 def render_tour(tour):
     tour_key = int(tour)
-    return render_template('tour.html', tour=tours[tour_key],
-                           departures=departures, title=title)
+    return render_template(
+        "tour.html", tour=tours[tour_key], departures=departures, title=title
+    )
 
 
-app.run('0.0.0.0', 8000)
+@app.errorhandler(500)
+def render_server_error(error):
+    return f"Что-то не так, но мы все починим, честное слово.", error
+
+
+@app.errorhandler(404)
+def render_404_error(error):
+    return render_template(
+        "404.html", background=background, departures=departures, title=title
+    )
+
+
+app.run("0.0.0.0", 8000)
